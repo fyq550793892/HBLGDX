0fd60b724082a796be9328dde9004853144dbb5b
Tony <tony@gmail.com>
2017-03-15 20:07:58 +0800

    weather data

---
+++ app/src/main/java/com/example/administrator/northchinauniversity/db/Province.java
@@ -0,0 +1,34 @@
+package com.example.administrator.northchinauniversity.db;
+
+import org.litepal.crud.DataSupport;
+import org.litepal.exceptions.DataSupportException;
+
+/**
+ * Created by Administrator on 2017/3/15.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;//省代号
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(){
+        this.provinceName=provinceName;
+    }
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode=provinceCode;
+    }
+
+
+}
